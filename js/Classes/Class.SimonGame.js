732f3f3d05608dfe0ab669f2ff6543510c83e61a
ItitErickBolanos <erick.bolanos@iest.edu.mx>
2016-05-30 22:54:01 -0500

    Finished coding the logic of the game
    Finished coding the game's logic, added sound. All that remains to be
    done is activate strict mode and notify the user both that he/she has
    won or made a mistake.

--- js/Classes/Class.SimonGame.js
+++ js/Classes/Class.SimonGame.js
@@ -1,26 +1,101 @@
 function SimonGame(){
+    this.gameOn = false;
     this.gameInProgress = false;
     this.strictMode = false;
     this.moveCounter = null;
     this.moveHistory = [];
+    this.playerMoves = [];
+    this.GAMEBUTTONS = ["green", "red", "yellow", "blue"];
+
 }
 
 SimonGame.prototype = {
-    start_game: function(){
-
+    getGamePower: function(){
+      return this.gameOn;
     },
 
-    stop_game: function(){
-
+    getGameState: function(){
+      return this.gameInProgress;
     },
 
-    activate_strict: function(){
-
+    getMoveCounter: function(){
+      return this.moveCounter;
     },
 
-    generate_moves: function(){
-
+    switchGameOn: function(){
+      this.gameOn = !this.gameOn;
+      if(!this.gameOn){
+        this.moveCounter = null;
+      }
+      return this.gameOn;
     },
+
+    startGame: function(){
+      this.gameInProgress = true;
+      this.moveCounter = null;
+      this.moveHistory = [];
+      this.playerMoves = [];
+      this.generateMoves();
+      this.showMoves();
+    },
+
+    activateStrict: function(){
+      this.strictMode = !this.strictMode;
+    },
+
+    generateMoves: function(){
+      var randomMove = Math.floor((Math.random() * 4));
+      this.moveCounter = this.moveHistory.length + 1;
+      this.moveHistory.push(this.GAMEBUTTONS[randomMove]);
+    },
+
+    showMoves: function(){
+      var that = this, i = 0;
+      for(var i = 0; i < this.moveHistory.length; i++){
+          this.lightUpButtons(i);
+      }
+    },
+
+    lightUpButtons: function(i){
+      var that = this;
+      setTimeout(function() {
+        var audio = document.getElementById(that.moveHistory[i] + 'Sound');
+        audio.play();
+        $("#" + that.moveHistory[i]).addClass(that.moveHistory[i]);
+        setTimeout(function() {
+            $("#" + that.moveHistory[i]).removeClass(that.moveHistory[i]);
+            audio.pause();
+            audio.currentTime = 0;
+        }, 700);
+      }, 1000 * i);
+    },
+
+    evaluatePlayerMoves: function(button){
+      this.playerMoves.push(button);
+      if (this.moveHistory[this.playerMoves.length - 1] != this.playerMoves[this.playerMoves.length - 1]){
+        this.showMistake();
+        return false;
+      }
+      if(this.playerMoves.length == this.moveHistory.length){
+        this.playerMoves = [];
+        return true;
+      }
+      return false;
+    },
+
+    showMistake: function(){
+      if(!this.strictMode){
+        this.playerMoves = [];
+        this.showMoves();
+      } else {
+        this.startGame();
+      }
+    },
+
+    showWin: function(){
+      console.log("YOU WIN!");
+      this.startGame();
+    }
 
 
 }
--- js/simonGame.js
+++ js/simonGame.js
@@ -1,5 +1,44 @@
 var app = angular.module("simonGame", ['ngMaterial']);
 
 app.controller("gameController", ['$scope', function($scope){
+  $scope.simon = new SimonGame();
+  $scope.gamePower = false;
+  $scope.counter = "- -";
 
+  $scope.evaluateMoves = function(button){
+    $scope.gameState = $scope.simon.getGameState();
+    if ($scope.gameState){
+      var audio = document.getElementById(button + 'Sound');
+      audio.play();
+      $scope.correctMove = $scope.simon.evaluatePlayerMoves(button);
+    }
+  };
+
+  $scope.nextMove = function(button){
+    if ($scope.correctMove){
+      $scope.counter = $scope.simon.getMoveCounter() < 10 ? "0" + $scope.simon.getMoveCounter() : $scope.simon.getMoveCounter();
+      if ($scope.counter == "20"){
+        $scope.simon.showWin();
+      } else {
+        $scope.simon.generateMoves();
+        $scope.simon.showMoves();
+      }
+    }
+    var audio = document.getElementById(button + 'Sound');
+    audio.pause();
+    audio.currentTime = 0;
+  }
+
+  $scope.switchGameOn = function(){
+    $scope.simon.switchGameOn();
+    $scope.gamePower = $scope.simon.getGamePower();
+    $scope.counter = "- -";
+  }
+
+  $scope.startGame = function(){
+    if ($scope.gamePower) {
+      $scope.simon.startGame();
+      $scope.counter = $scope.simon.getMoveCounter() < 10 ? "0" + $scope.simon.getMoveCounter() : $scope.simon.getMoveCounter();
+    }
+  }
 }]);
